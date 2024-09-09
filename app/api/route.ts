683e0a0cd5a4b29ad9fde41940962049dda50d9a
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2024-09-09 22:28:50 +0700

    fix: update promt for convert message

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,30 +4,6 @@ const { GoogleGenerativeAI } = require('@google/generative-ai')
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
-export async function GET(request: NextRequest) {
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
-
-  const message = 'ฉันยังยุ่งอยู่ ไม่อยากออกไปตอนนี้'
-
-  const result = await model.generateContent(`แปลงคำว่า ${message} ให้เป็นคำสุภาพที่ใช้ในการสนทนา`)
-
-  const response = await result.response
-  const text = response.text()
-
-  return new Response(
-    JSON.stringify({
-      message: 'Success',
-      data: text,
-    }),
-    {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  )
-}
-
 export async function POST(request: NextRequest) {
   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
 
@@ -35,9 +11,9 @@ export async function POST(request: NextRequest) {
 
   const { message } = data
 
-  console.log(message)
-
-  const result = await model.generateContent(`แปลงคำว่า ${message} ให้เป็นคำสุภาพที่ใช้ในการสนทนา`)
+  const result = await model.generateContent(
+    `Convert ${message} into polite conversational words and return only the message without reason by thai language`
+  )
 
   const response = await result.response
 
